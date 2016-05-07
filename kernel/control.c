3af4d1b51566a3a349fd74057a682a8fa6f866bb
pe64 <55121593@qq.com>
2016-05-07 09:44:38 -0400

    align

--- kernel/control.c
+++ kernel/control.c
@@ -12,7 +12,7 @@ MODULE_LICENSE("GPL");
 
 #ifdef INTERCEPT_DEBUG
 #define dbgprint(format,args...) \
-		printk("intercept: function:%s-L%d: "format, __FUNCTION__, __LINE__, ##args);
+	printk("intercept: function:%s-L%d: "format, __FUNCTION__, __LINE__, ##args);
 #else
 #define dbgprint(format,args...) do {} while(0);
 #endif
@@ -39,22 +39,22 @@ long new_execve(char __user *name, char __user * __user *argv,
 /** do_execve and do_fork */
 unsigned int can_exec_fork = 0;
 int (*new_do_execve) (char * filename,
-				char __user *__user *argv,
-				char __user *__user *envp,
-				struct pt_regs * regs);
+		char __user *__user *argv,
+		char __user *__user *envp,
+		struct pt_regs * regs);
 
 
 struct idtr {
-		unsigned short limit;
-		unsigned int base;
+	unsigned short limit;
+	unsigned int base;
 } __attribute__ ((packed));
 
 
 struct idt {
-		unsigned short off1;
-		unsigned short sel;
-		unsigned char none, flags;
-		unsigned short off2;
+	unsigned short off1;
+	unsigned short sel;
+	unsigned char none, flags;
+	unsigned short off2;
 } __attribute__ ((packed));
 
 
@@ -67,32 +67,32 @@ struct idt {
 struct kprobe kp_exec;
 unsigned int can_intercept_fork_exec(void)
 {
-		int ret = 0;
+	int ret = 0;
 
 #ifndef CONFIG_KPROBES
-		return ret;
+	return ret;
 #endif
 
-		kp_exec.symbol_name = "do_execve";
+	kp_exec.symbol_name = "do_execve";
 
-		ret = register_kprobe(&kp_exec);
-		if (ret != 0 ) {
-				dbgprint("cannot find do_execve by kprobe.\n");
-				return 0;
-		}
-		new_do_execve = ( int (*)
-						(char *,
-						 char __user * __user *,
-						 char __user * __user *,
-						 struct pt_regs *
-						)
-						) kp_exec.addr;
+	ret = register_kprobe(&kp_exec);
+	if (ret != 0 ) {
+		dbgprint("cannot find do_execve by kprobe.\n");
+		return 0;
+	}
+	new_do_execve = ( int (*)
+			(char *,
+			 char __user * __user *,
+			 char __user * __user *,
+			 struct pt_regs *
+			)
+			) kp_exec.addr;
 
-		dbgprint("do_execve at %p\n", (void *)kp_exec.addr);
-		unregister_kprobe(&kp_exec);
+	dbgprint("do_execve at %p\n", (void *)kp_exec.addr);
+	unregister_kprobe(&kp_exec);
 
 
-		return 1;
+	return 1;
 }
 
 
@@ -102,22 +102,22 @@ unsigned int can_intercept_fork_exec(void)
  * */
 unsigned int clear_and_return_cr0(void)
 {
-		unsigned int cr0 = 0;
-		unsigned int ret;
+	unsigned int cr0 = 0;
+	unsigned int ret;
 
-		asm volatile ("movl %%cr0, %%eax"
-						: "=a"(cr0)
-					 );
-		ret = cr0;
+	asm volatile ("movl %%cr0, %%eax"
+			: "=a"(cr0)
+		     );
+	ret = cr0;
 
-		/* clear the 20 bit of CR0, a.k.a WP bit */
-		cr0 &= 0xfffeffff;
+	/* clear the 20 bit of CR0, a.k.a WP bit */
+	cr0 &= 0xfffeffff;
 
-		asm volatile ("movl %%eax, %%cr0"
-						:
-						: "a"(cr0)
-					 );
-		return ret;
+	asm volatile ("movl %%eax, %%cr0"
+			:
+			: "a"(cr0)
+		     );
+	return ret;
 }
 
 /** set CR0 with new value
@@ -126,10 +126,10 @@ unsigned int clear_and_return_cr0(void)
  * */
 void setback_cr0(unsigned int val)
 {
-		asm volatile ("movl %%eax, %%cr0"
-						:
-						: "a"(val)
-					 );
+	asm volatile ("movl %%eax, %%cr0"
+			:
+			: "a"(val)
+		     );
 }
 
 
@@ -137,31 +137,31 @@ void setback_cr0(unsigned int val)
  * * Return the first appearence of NEEDLE in HAYSTACK. 
  * * */
 static void *memmem(const void *haystack, size_t haystack_len,
-				const void *needle, size_t needle_len)
+		const void *needle, size_t needle_len)
 {/*{{{*/
-		const char *begin;
-		const char *const last_possible
-				= (const char *) haystack + haystack_len - needle_len;
+	const char *begin;
+	const char *const last_possible
+		= (const char *) haystack + haystack_len - needle_len;
 
-		if (needle_len == 0)
-				/* The first occurrence of the empty string is deemed to occur at
-				 *            the beginning of the string. */
-				return (void *) haystack;
+	if (needle_len == 0)
+		/* The first occurrence of the empty string is deemed to occur at
+		 *            the beginning of the string. */
+		return (void *) haystack;
 
-		/* Sanity check, otherwise the loop might search through the whole
-		 *        memory. */
-		if (__builtin_expect(haystack_len < needle_len, 0))
-				return NULL;
-
-		for (begin = (const char *) haystack; begin <= last_possible;
-						++begin)
-				if (begin[0] == ((const char *) needle)[0]
-								&& !memcmp((const void *) &begin[1],
-										(const void *) ((const char *) needle + 1),
-										needle_len - 1))
-						return (void *) begin;
-
+	/* Sanity check, otherwise the loop might search through the whole
+	 *        memory. */
+	if (__builtin_expect(haystack_len < needle_len, 0))
 		return NULL;
+
+	for (begin = (const char *) haystack; begin <= last_possible;
+			++begin)
+		if (begin[0] == ((const char *) needle)[0]
+				&& !memcmp((const void *) &begin[1],
+					(const void *) ((const char *) needle + 1),
+					needle_len - 1))
+			return (void *) begin;
+
+	return NULL;
 }/*}}}*/
 
 
@@ -170,47 +170,47 @@ static void *memmem(const void *haystack, size_t haystack_len,
  * */
 static unsigned long get_sys_call_table(void)
 {/*{{{*/
-		/* we'll read first 100 bytes of int $0x80 */
+	/* we'll read first 100 bytes of int $0x80 */
 #define OFFSET_SYSCALL 100 
 
-		struct idtr idtr;
-		struct idt idt;
-		unsigned sys_call_off;
-		unsigned retval;
-		char sc_asm[OFFSET_SYSCALL], *p;
+	struct idtr idtr;
+	struct idt idt;
+	unsigned sys_call_off;
+	unsigned retval;
+	char sc_asm[OFFSET_SYSCALL], *p;
 
-		/* well, let's read IDTR */
-		asm("sidt %0":"=m"(idtr)
-						:
-						:"memory" );
+	/* well, let's read IDTR */
+	asm("sidt %0":"=m"(idtr)
+			:
+			:"memory" );
 
-		dbgprint("idtr base at 0x%X\n", (unsigned int)idtr.base);
+	dbgprint("idtr base at 0x%X\n", (unsigned int)idtr.base);
 
-		/* Read in IDT for vector 0x80 (syscall) */
-		memcpy(&idt, (char *) idtr.base + 8 * 0x80, sizeof(idt));
+	/* Read in IDT for vector 0x80 (syscall) */
+	memcpy(&idt, (char *) idtr.base + 8 * 0x80, sizeof(idt));
 
-		sys_call_off = (idt.off2 << 16) | idt.off1;
+	sys_call_off = (idt.off2 << 16) | idt.off1;
 
-		dbgprint("idt80: flags=%X sel=%X off=%X\n",
-						(unsigned) idt.flags, (unsigned) idt.sel, sys_call_off);
+	dbgprint("idt80: flags=%X sel=%X off=%X\n",
+			(unsigned) idt.flags, (unsigned) idt.sel, sys_call_off);
 
-		/* we have syscall routine address now, look for syscall table
-		 *        dispatch (indirect call) */
-		memcpy(sc_asm, (void *)sys_call_off, OFFSET_SYSCALL);
+	/* we have syscall routine address now, look for syscall table
+	 *        dispatch (indirect call) */
+	memcpy(sc_asm, (void *)sys_call_off, OFFSET_SYSCALL);
 
-		/**
-		 *      * Search opcode of `call sys_call_table(,eax,4)'
-		 *           */
-		p = (char *) memmem(sc_asm, OFFSET_SYSCALL, "\xff\x14\x85", 3);
-		if (p == NULL)
-				return 0;
+	/**
+	 *      * Search opcode of `call sys_call_table(,eax,4)'
+	 *           */
+	p = (char *) memmem(sc_asm, OFFSET_SYSCALL, "\xff\x14\x85", 3);
+	if (p == NULL)
+		return 0;
 
-		retval = *(unsigned *) (p + 3);
-		if (p) {
-				dbgprint("sys_call_table at 0x%x, call dispatch at 0x%x\n",
-								retval, (unsigned int) p);
-		}
-		return retval;
+	retval = *(unsigned *) (p + 3);
+	if (p) {
+		dbgprint("sys_call_table at 0x%x, call dispatch at 0x%x\n",
+				retval, (unsigned int) p);
+	}
+	return retval;
 #undef OFFSET_SYSCALL
 }/*}}}*/
 
@@ -222,8 +222,8 @@ static unsigned long get_sys_call_table(void)
  * */
 asmlinkage long new_open(char *filename, int flags, int mode)
 {
-		dbgprint("hello\n");
-		return old_open (filename, flags, mode);
+	dbgprint("hello\n");
+	return old_open (filename, flags, mode);
 }
 
 # ifdef __i386__
@@ -233,36 +233,36 @@ asmlinkage long new_open(char *filename, int flags, int mode)
  * */
 asmlinkage int new_execve(struct pt_regs regs)
 {
-		int error;
-		char *filename;
+	int error;
+	char *filename;
 
-		dbgprint("Hello\n");
+	dbgprint("Hello\n");
 
-		filename = getname((char __user *)regs.bx );
-		if(strstr(filename,"vmware")){
-			error = PTR_ERR((void*)-EIO);
-			goto out;
-		}
-		error = PTR_ERR(filename);
-		if ( IS_ERR(filename) )
-				goto out;
-		dbgprint("file to execve: %s\n", filename);
-		error = new_do_execve(filename,
-						(char __user * __user *) regs.cx,
-						(char __user * __user *) regs.dx,
-						&regs);
-		if (error == 0) {
-				task_lock(current);
-				current->ptrace &= ~PT_DTRACE;
-				task_unlock(current);
-				set_thread_flag(TIF_IRET);
-		}
-		putname((const char*)filename);
+	filename = getname((char __user *)regs.bx );
+	if(strstr(filename,"vmware")){
+		error = PTR_ERR((void*)-EIO);
+		goto out;
+	}
+	error = PTR_ERR(filename);
+	if ( IS_ERR(filename) )
+		goto out;
+	dbgprint("file to execve: %s\n", filename);
+	error = new_do_execve(filename,
+			(char __user * __user *) regs.cx,
+			(char __user * __user *) regs.dx,
+			&regs);
+	if (error == 0) {
+		task_lock(current);
+		current->ptrace &= ~PT_DTRACE;
+		task_unlock(current);
+		set_thread_flag(TIF_IRET);
+	}
+	putname((const char*)filename);
 out:
-		return error;
+	return error;
 }
 #else
-asmlinkage
+	asmlinkage
 long new_execve(char __user *name, char __user * __user *argv,
 		char __user * __user *envp, struct pt_regs *regs)
 {
@@ -280,47 +280,47 @@ long new_execve(char __user *name, char __user * __user *argv,
 #endif
 static int intercept_init(void)
 {
-		my_table = (void **)get_sys_call_table();
-		if (my_table == NULL)
-				return -1;
+	my_table = (void **)get_sys_call_table();
+	if (my_table == NULL)
+		return -1;
 
-		dbgprint("sys call table address %p\n", (void *) my_table);
+	dbgprint("sys call table address %p\n", (void *) my_table);
 
 #define REPLACE(x) old_##x = my_table[__NR_##x];\
-		my_table[__NR_##x] = new_##x
+	my_table[__NR_##x] = new_##x
 
 
-		//REPLACE(open);
-		can_exec_fork = can_intercept_fork_exec();
-		if(can_exec_fork == 1)
-				REPLACE(execve);
+	//REPLACE(open);
+	can_exec_fork = can_intercept_fork_exec();
+	if(can_exec_fork == 1)
+		REPLACE(execve);
 
 #undef REPLACE
-		return 0;
+	return 0;
 }
 
 static int __init this_init(void)
 {
-		int ret;
-		printk("syscall intercept: Hi, poor linux!\n");
+	int ret;
+	printk("syscall intercept: Hi, poor linux!\n");
 
-		orig_cr0 = clear_and_return_cr0(); 
-		ret = intercept_init();
-		setback_cr0(orig_cr0);
+	orig_cr0 = clear_and_return_cr0(); 
+	ret = intercept_init();
+	setback_cr0(orig_cr0);
 
-		return ret;
+	return ret;
 }
 
 static void __exit this_fini(void)
 {
-		printk("syscall intercept: bye, poor linux!\n");
+	printk("syscall intercept: bye, poor linux!\n");
 
 #define RESTORE(x) my_table[__NR_##x] = old_##x
 
-		orig_cr0 = clear_and_return_cr0(); 
-		if(can_exec_fork == 1)
-				RESTORE(execve);
-		setback_cr0(orig_cr0);
+	orig_cr0 = clear_and_return_cr0(); 
+	if(can_exec_fork == 1)
+		RESTORE(execve);
+	setback_cr0(orig_cr0);
 
 #undef RESTORE
 }
