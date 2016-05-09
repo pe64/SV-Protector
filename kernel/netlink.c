588c21f560af0399f553a151614c639c9f1683cc
pe64 <55121593@qq.com>
2016-05-09 16:36:48 +0800

    netlink

--- kernel/netlink.c
+++ kernel/netlink.c
@@ -1,11 +1,27 @@
 #include <linux/ftrace.h>
 #include <linux/kprobes.h>
 #include <asm/unistd.h>
+#include <net/netlink.h>
+
+
+
+void input(struct sk_buff *skb)
+{
+	mutex_lock(&sv_mutex);
+	__input(skb);
+	mutex_unlock(&sv_mutex);
+}
+
+struct sock *sk;
 
 int netlink_init(void)
 {
 	printk("hello netlink");
-
+	
+	sk = netlink_kernel_create(&init_net, NETLINK_SV_PROTECT, 1, input, NULL, THIS_MODULE);
+	if(!sk){
+		printk("netlink create error !!!!");
+	}
 
 	return 0;
 }
