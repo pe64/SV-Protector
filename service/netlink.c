ab61230af8447c0ee288524841a673357a092f69
pe64 <55121593@qq.com>
2016-05-10 13:11:54 +0800

    modify netlink

--- service/netlink.c
+++ service/netlink.c
@@ -2,36 +2,64 @@
 #include <asm/types.h>
 #include <linux/netlink.h>
 
-int sk_netlink_fd;
-struct sockaddr_nl nl_addr;
+#define BIND_NLADDR_FLAGS 0
+#define SEND_NLADDR_FLAGS 1
 
-int init_netlink_socket(void)
+struct sockaddr_nl *
+init_sockaddr_nl_object(int flags)
 {
+	struct sockaddr_nl *addr;
+
+	addr = (struct sockaddr_nl *)malloc(sizeof(struct sockaddr_nl));
+	if(!addr){
+		return addr;
+	}
+
+	addr->nl_family = AF_NETLINK;
+	if(flags == BIND_NLADDR_FLAGS){
+		addr->nl_pid = getpid();
+	}else{
+		addr->nl_pid = 0;	
+	}
+	return addr;
+}
+
+int init_netlink_socket(struct sockaddr_nl *nl_addr)
+{
+	int sk_netlink_fd;
+	
+	if(!addr){
+		return 0;
+	}
+
 	sk_netlink_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_PROTECT);
 	if(sk_netlink_fd <= 0){
-		sk_netlink_fd = 0;
-		return -1;
+		return sk_netlink_fd;
 	}
 
-	nl_addr.nl_family = AF_NETLINK;
-	nl_addr.nl_pid = getpid();
-
-	if(bind(sk_netlink_fd, (struct sockaddr *)&nl_addr, sizeof(struct sockaddr_nl))){
+	if(bind(sk_netlink_fd, (struct sockaddr *)nl_addr, sizeof(struct sockaddr_nl))){
 		close(sk_netlink_fd);
 		sk_netlink_fd = 0;
-		return SQUID_ERROR;
 	}
-	nl_addr.nl_pid = 0;
-	return 0;
+
+	return sk_netlink_fd;
 }
 
 struct nlmsghdr *build_netlink_msg(struct msghdr *msg, struct iovec *iov)
 {
 	struct nlmsghdr *nlh;
+	struct sockaddr_nl *send_addr;
+	
+	nlh = NULL;
+
+	send_addr = init_sockaddr_nl_object(SEND_NLADDR_FLAGS);
+	if(!send_addr){
+		goto out;
+	}
 
 	nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(sizeof(sqnl_msg_st)));
 	if(!nlh){
-		return nlh;
+		goto out;
 	}
 
 	bzero(nlh, NLMSG_SPACE(sizeof(sqnl_msg_st)));
@@ -42,11 +70,13 @@ struct nlmsghdr *build_netlink_msg(struct msghdr *msg, struct iovec *iov)
 	iov->iov_base = nlh;
 	iov->iov_len = nlh->nlmsg_len;
 
-	msg->msg_name = &nl_addr;
+	msg->msg_name = nl_addr;
 	msg->msg_namelen = sizeof(struct sockaddr_nl);
 	msg->msg_iov = iov;
 	msg->msg_iovlen = 1;
-
+out:
+	if(send_addr)
+		free(send_addr);
 	return nlh;
 }
 
