e5887b5f8ad1c78c598fe77f7c7a771e42cb41a8
pe64 <55121593@qq.com>
2016-05-25 16:26:43 +0800

    file

--- include/sv_file.h
+++ include/sv_file.h
@@ -1,8 +1,12 @@
 #ifndef SV_FILE_H
 #define SV_FILE_H
 
+#include <linux/list.h>
+#include <linux/coda.h>
+
 typedef struct {
 	char file_name[256];
+	ino_t inode;
 	pid_t pid;
 	int flags;
 }svfile_u2k_args_st;
@@ -10,7 +14,18 @@ typedef struct {
 typedef struct {
 	int err;
 	char file_name[256];
+	ino_t inode;
 	int index;
 }svfile_k2u_ret_st;
+
+typedef struct {
+	struct list_head list;
+	char name[256];
+	ino_t inode;
+	int index;
+	int flags;
+	pid_t write_pid;
+	time_t timestamps;
+}svfile_list_st;
 
 #endif
--- kernel/sv_file.c
+++ kernel/sv_file.c
@@ -1,13 +1,47 @@
 #include <asm/unistd.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/gfp.h>
+#include <linux/slob_def.h>
 
 #include "sv_base.h"
 #include "sv_file.h"
 #include "sv_frame.h"
 #include "priv.h"
 
-const struct file_operations *get_file_operations(char *path)
+static svfile_list_st s_svfile_head;
+static struct file_operations *s_fops;
+
+static long (*system_unlock_ioctl)(struct file *, unsigned int, unsigned long);
+static long (*system_compat_ioctl)(struct file *, unsigned int, unsigned long);
+
+static void init_svfile_list_node(svfile_list_st *node)
+{
+	memset(node, 0, sizeof(svfile_list_st));
+	INIT_LIST_HEAD(&node->list);
+}
+
+static long sv_file_protect_unlock_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	return system_unlock_ioctl(filp, cmd, arg);
+}
+
+static long sv_file_protect_compat_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	return system_compat_ioctl(filp, cmd, arg);
+}
+
+static void replace_fops_ioctl(struct file_operations *f_op)
+{
+	if(f_op){
+		f_op->unlocked_ioctl = sv_file_protect_unlock_ioctl;
+#ifdef CONFIG_COMPAT
+		f_op->compat_ioctl = sv_file_protect_compat_ioctl;
+#endif
+	}
+}
+
+static struct file_operations *get_file_operations(char *path)
 {
 	struct file *filp;
 	struct file_operations *f_op;
@@ -25,7 +59,24 @@ const struct file_operations *get_file_operations(char *path)
 
 int svfile_add_protect_file(svfile_u2k_args_st *args)
 {
-	printk("[%s][%d] file[%s]\n",__FILE__,__LINE__, args->file_name);
+	//printk("[%s][%d] file[%s]\n",__FILE__,__LINE__, args->file_name);
+	svfile_list_st *node;
+
+	if(!s_fops){
+		s_fops = get_file_operations(args->file_name);
+		if(!s_fops){
+			return SV_ERROR;
+		}
+		replace_fops_ioctl(s_fops);
+	}
+	
+	node = (svfile_list_st *)kmalloc(sizeof(svfile_list_st), GFP_KERNEL);
+	if(node){
+		printk("kmalloc error!!!!");
+		return SV_ERROR;
+	}
+
+	init_svfile_list_node(node);
 
 	return SV_OK;
 }
@@ -39,6 +90,7 @@ int file_init(void)
 
 	printk("file init!!!");
 	svframe_register_kernel_syscall(&file_ops);
+	INIT_LIST_HEAD(&s_svfile_head.list);
 
 	return 0;
 }
