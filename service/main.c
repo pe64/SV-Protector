19c9b7bad76bda2ed12eecc1529ade05d47ab184
pe64 <55121593@qq.com>
2016-07-19 16:23:15 +0800

    service

--- service/main.c
+++ service/main.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <linux/fs.h>
 
 #include "sv_file.h"
 #include "sv_base.h"
@@ -15,17 +16,21 @@ int main(int argc, char **argv)
 	int ret;
 	sv_kernel_req_st req;
 	struct stat st;
+	int fd;
+	int arg = FS_IMMUTABLE_FL;
 
 	bzero(&st, sizeof(st));
 	bzero(&req, sizeof(sv_kernel_req_st));
 	req.module_cmd = SV_FILE_REQ_ADD;
-	req.set.fd = open("a",O_RDWR);
+	fd = open("a",O_RDWR);
+	req.set.fd = fd;
 	fstat(req.set.fd, &st);
 	req.set.inode = st.st_ino;
 	strcpy(req.set.password, "wxfqwer");
 
 	sv_syscall_init();
 	sv_syscall_invoke(SV_FRAME_PROTECT_FILE, &req);
-	
+	ioctl(fd, FS_IOC_SETFLAGS, &arg);
+
 	return 0;
 }
